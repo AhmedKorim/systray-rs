33f08928eadce264e9a0698b2c142765f9f734d7
ahmed korim <ahmedkorrim@gmail.com>
2020-09-01 22:00:40 +0200

    shit wont work

--- examples/trayicon.rs
+++ examples/trayicon.rs
@@ -1,51 +1,69 @@
 #![windows_subsystem = "windows"]
 
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time ::Duration;
+
 #[cfg(not(target_os = "macos"))]
 fn main() -> Result<(), systray::Error> {
-    let mut app;
-    match systray::Application::new() {
-        Ok(w) => app = w,
-        Err(_) => panic!("Can't create window!"),
-    }
-    // w.set_icon_from_file(&"C:\\Users\\qdot\\code\\git-projects\\systray-rs\\resources\\rust.ico".to_string());
-    // w.set_tooltip(&"Whatever".to_string());
-    app.set_icon_from_file("/usr/share/gxkb/flags/ua.png")?;
+	let mut app;
+	match systray::Application::new() {
+		Ok(w) => app = w,
+		Err(_) => panic!("Can't create window!"),
+	}
+	// w.set_icon_from_file(&"C:\\Users\\qdot\\code\\git-projects\\systray-rs\\resources\\rust.ico".to_string());
+	// w.set_tooltip(&"Whatever".to_string());
+	app.set_icon_from_file("/usr/share/gxkb/flags/ua.png")?;
 
-    app.add_menu_item("Print a thing", |_| {
-        println!("Printing a thing!");
-        Ok::<_, systray::Error>(())
-    })?;
+	app.add_menu_item("Print a thing", |_| {
+		println!("Printing a thing!");
+		Ok::<_, systray::Error>(())
+	})?;
 
-    app.add_menu_item("Add Menu Item", |window| {
-        window.add_menu_item("Interior item", |_| {
-            println!("what");
-            Ok::<_, systray::Error>(())
-        })?;
-        window.add_menu_separator()?;
-        Ok::<_, systray::Error>(())
-    })?;
+	app.add_menu_item("Add Menu Item", |window| {
+		window.add_menu_item("Interior item", |_| {
+			println!("what");
+			Ok::<_, systray::Error>(())
+		})?;
+		window.add_menu_separator()?;
+		Ok::<_, systray::Error>(())
+	})?;
 
-    app.add_menu_separator()?;
+	app.add_menu_separator()?;
 
-    app.add_menu_item("Quit", |window| {
-        window.quit();
-        Ok::<_, systray::Error>(())
-    })?;
+	app.add_menu_item("Quit", |window| {
+		window.quit();
+		Ok::<_, systray::Error>(())
+	})?;
 
-    println!("Waiting on message!");
-    app.wait_for_message()?;
-    Ok(())
+	println!("Waiting on message!");
+	app.wait_for_message()?;
+	Ok(())
 }
 
 #[cfg(target_os = "macos")]
 fn main() {
-    let mut app;
-    match systray::Application::new() {
-        Ok(w) => app = w,
-        Err(e) => panic!("Can't create tray icon app!")
-    }
+	let mut app = match systray::Application::new() {
+		Ok(w) => w,
+		Err(e) => {
+			dbg!(e);
+			panic!("Can't create tray icon app!")
+		}
+	};
+	let app = Arc::new(Mutex::new(app));
+	let app_0 = app.clone();
+	dbg!("pre spawn");
+	thread::spawn(move || {
+		const ICON_BUFFER: &'static [u8] = include_bytes!("rust-logo.png");
+		{
+			dbg!("running appp");
+			thread::sleep(Duration::from_secs(5));
+			dbg!("settings icon");
+			app_0.lock().unwrap().set_icon_from_buffer(ICON_BUFFER, 256, 256).unwrap();
+		}
+	});
 
-    const ICON_BUFFER: &'static [u8] = include_bytes!("rust-logo.png");
-    app.set_icon_from_buffer(ICON_BUFFER, 256, 256).unwrap();
-    app.wait_for_message();
+	dbg!("App will run now!");
+
+	app.lock().unwrap().run();
 }
--- src/api/cocoa/mod.rs
+++ src/api/cocoa/mod.rs
@@ -1,113 +1,177 @@
 //! Contains the implementation of the Mac OS X tray icon in the top bar.
 
-use std::{
-    self,
-    sync::mpsc::Sender
-};
+use std::{self, sync::mpsc::Sender, thread};
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{channel, Receiver};
+use std::thread::JoinHandle;
+use std::time::Duration;
+
 use cocoa::{
-    appkit::{NSApp, NSApplication, NSButton, NSImage, NSStatusBar, NSStatusItem,
-             NSSquareStatusItemLength},
-    base::{id, nil},
-    foundation::{NSData, NSSize, NSAutoreleasePool}
+	appkit::{NSApp, NSApplication, NSButton, NSImage, NSSquareStatusItemLength, NSStatusBar,
+			 NSStatusItem},
+	base::{id, nil},
+	foundation::{NSAutoreleasePool, NSData, NSSize},
 };
+use cocoa::appkit::NSApplicationActivationPolicy::NSApplicationActivationPolicyRegular;
+use objc::runtime::Object;
+
 use crate::{Error, SystrayEvent};
+
+// safily move object between threads
+#[derive(Clone, Debug)]
+pub struct SafeId(Arc<Mutex<*mut Object>>);
+
+impl SafeId {
+	fn new(id: id) -> Self {
+		SafeId(Arc::new(Mutex::new(id)))
+	}
+}
+
+unsafe impl Send for SafeId {}
+
+unsafe impl Sync for SafeId {}
+
+
+/// event for comunicated with the app running on the main thread
+pub enum OsxSystemTrayEvent {
+	/// don't allocate for the image buffer
+	ChangeImage(&'static [u8]),
+	Shutdown,
+}
+
+const ICON_WIDTH: f64 = 32.0;
+const ICON_HEIGHT: f64 = 32.0;
 
 /// The generation representation of the Mac OS X application.
 pub struct Window {
-    /// A mutable reference to the `NSApplication` instance of the currently running application.
-    application: id,
-    /// It seems that we have to use `NSAutoreleasePool` to prevent memory leaks.
-    autorelease_pool: id,
+	/// A mutable reference to the `NSApplication` instance of the currently running application.
+	application: SafeId,
+	/// It seems that we have to use `NSAutoreleasePool` to prevent memory leaks.
+	autorelease_pool: SafeId,
+	tray: SafeId,
+	/// sender for the wrapper
+	event_tx: Sender<SystrayEvent>,
+	/// the handler will be the sender for the running app which is consuming the main thread
+	handler: Sender<OsxSystemTrayEvent>,
 }
 
 impl Window {
-    /// Creates a new instance of the `Window`.
-    pub fn new(event_tx: Sender<SystrayEvent>) -> Result<Window, Error> {
-        Ok(Window {
-            application: unsafe { NSApp() },
-            autorelease_pool: unsafe { NSAutoreleasePool::new(nil) },
-        })
-    }
+	/// Creates a new instance of the `Window`.
+	pub fn new(event_tx: Sender<SystrayEvent>) -> Result<Window, Error> {
+		let (handler_tx, handler_rx) = channel();
+		let mut app = unsafe { NSApp() };
+		unsafe { app.setActivationPolicy_(NSApplicationActivationPolicyRegular); }
+		let bool = unsafe { NSAutoreleasePool::new(nil) };
+		let mut window = Window {
+			application: unsafe { SafeId::new(NSApp()) },
+			autorelease_pool: SafeId::new(bool),
+			tray: SafeId::new(Window::init_tray()),
+			event_tx,
+			handler: handler_tx,
+		};
+		let lister_thread = window.run_lister(handler_rx);
+		Ok(window)
+	}
+	fn init_tray() -> id {
+		unsafe {
+			NSStatusBar::systemStatusBar(nil)
+				.statusItemWithLength_(NSSquareStatusItemLength)
+				.autorelease()
+		}
+	}
+	pub(crate) fn run(&mut self) {
+		unsafe {
+			self.application.clone().0.lock().unwrap().run();
+		}
+	}
+	fn run_lister(&mut self, rx: Receiver<OsxSystemTrayEvent>) -> JoinHandle<()> {
+		let tray = self.tray.clone();
+		thread::spawn(move || loop {
+			let lister = rx.try_iter();
+			for e in lister {
+				match e {
+					OsxSystemTrayEvent::ChangeImage(image) => unsafe {
+						let nsdata = NSData::dataWithBytes_length_(
+							nil,
+							image.as_ptr() as *const std::os::raw::c_void,
+							image.len() as u64,
+						)
+							.autorelease();
 
-    /// Closes the current application.
-    pub fn quit(&self) {
-        let _: () = unsafe { msg_send![self.application, terminate] };
-    }
+						let nsimage = unsafe {
+							NSImage::initWithData_(NSImage::alloc(nil), nsdata).autorelease()
+						};
+						let new_size = NSSize::new(ICON_WIDTH, ICON_HEIGHT);
 
-    /// Sets the tooltip (not available for this platfor).
-    pub fn set_tooltip(&self, _: &str) -> Result<(), Error> {
-        Err(Error::OsError("This operating system does not support tooltips for the tray \
+						let r: () = msg_send![nsimage, setSize: new_size];
+						tray.0.lock().unwrap().button().setImage_(nsimage);
+					},
+					OsxSystemTrayEvent::Shutdown => {
+						unimplemented!();
+					}
+				}
+			}
+		})
+	}
+	/// Closes the current application.
+	pub fn quit(&self) {
+		// let app = self.application.0.clone().lock().unwrap();
+		// let _: () = unsafe { msg_send![app, terminate] };
+		unimplemented!()
+	}
+
+	/// Sets the tooltip (not available for this platfor).
+	pub fn set_tooltip(&self, _: &str) -> Result<(), Error> {
+		Err(Error::OsError("This operating system does not support tooltips for the tray \
                                    items".to_owned()))
-    }
+	}
 
-    /// Adds an additional item to the tray icon menu.
-    pub fn add_menu_item<F>(&self, _: &String, _: F) -> Result<u32, Error>
-        where F: std::ops::Fn(&Window) -> () + 'static
-    {
-        unimplemented!()
-    }
+	/// Adds an additional item to the tray icon menu.
+	pub fn add_menu_item<F>(&self, _: &String, _: F) -> Result<u32, Error>
+		where F: std::ops::Fn(&Window) -> () + 'static
+	{
+		unimplemented!()
+	}
 
-    /// Sets the application icon displayed in the tray bar. Accepts a `buffer` to the underlying
-    /// image, you can pass even encoded PNG images here. Supports the same list of formats as
-    /// `NSImage`.
-    pub fn set_icon_from_buffer(&mut self, buffer: &'static [u8], _: u32, _: u32)
-        -> Result<(), Error>
-    {
-        const ICON_WIDTH: f64 = 18.0;
-        const ICON_HEIGHT: f64 = 18.0;
+	/// Sets the application icon displayed in the tray bar. Accepts a `buffer` to the underlying
+	/// image, you can pass even encoded PNG images here. Supports the same list of formats as
+	/// `NSImage`.
+	pub fn set_icon_from_buffer(&mut self, buffer: &'static [u8], _: u32, _: u32)
+								-> Result<(), Error> {
+		dbg!(buffer);
+		self.handler.send(OsxSystemTrayEvent::ChangeImage(buffer)).unwrap();
+		Ok(())
+	}
 
-        let tray_entry = unsafe {
-            NSStatusBar::systemStatusBar(nil).statusItemWithLength_(NSSquareStatusItemLength)
-                                             .autorelease()
-        };
+	/// Starts the application event loop. Calling this function will block the current thread.
+	pub fn wait_for_message(&mut self) -> Result<(),()>{
+		thread::spawn(move || {
+			loop {
+				log::debug!("listinging ..");
+				thread::sleep(Duration::from_millis(100));
+			}
+		});
 
-        let nsdata = unsafe {
-            NSData::dataWithBytes_length_(nil,
-                                          buffer.as_ptr() as *const std::os::raw::c_void,
-                                          buffer.len() as u64).autorelease()
-        };
-        if nsdata == nil {
-            return Err(Error::OsError("Could not create `NSData` out of the passed buffer"
-                                             .to_owned()));
-        }
+		Ok(())
+	}
 
-        let nsimage = unsafe { NSImage::initWithData_(NSImage::alloc(nil), nsdata).autorelease() };
-        if nsimage == nil {
-            return Err(Error::OsError("Could not create `NSImage` out of the created \
-                                             `NSData` buffer".to_owned()));
-        }
+	pub fn set_icon_from_resource(&self, resource_name: &str) -> Result<(), Error> {
+		unimplemented!()
+	}
 
-        unsafe {
-            let new_size = NSSize::new(ICON_WIDTH, ICON_HEIGHT);
-            let _: () = msg_send![nsimage, setSize:new_size];
-            tray_entry.button().setImage_(nsimage);
-        }
+	pub fn set_icon_from_file(&self, icon_file: &str) -> Result<(), Error> {
+		unimplemented!()
+	}
 
-        Ok(())
-    }
+	pub fn add_menu_separator(&self, item_idx: u32) -> Result<(), Error> {
+		unimplemented!()
+	}
 
-    /// Starts the application event loop. Calling this function will block the current thread.
-    pub fn wait_for_message(&mut self) {
-        unsafe { self.application.run() };
-    }
+	pub fn add_menu_entry(&self, item_idx: u32, item_name: &str) -> Result<(), Error> {
+		unimplemented!()
+	}
 
-    pub fn set_icon_from_resource(&self, resource_name: &str) -> Result<(), Error> {
-        unimplemented!()
-    }
-
-    pub fn set_icon_from_file(&self, icon_file: &str) -> Result<(), Error> {
-        unimplemented!()
-    }
-
-    pub fn add_menu_separator(&self, item_idx: u32) -> Result<(), Error> {
-        unimplemented!()
-    }
-
-    pub fn add_menu_entry(&self, item_idx: u32, item_name: &str) -> Result<(), Error> {
-        unimplemented!()
-    }
-
-    pub fn shutdown(&self) -> Result<(), Error> {
-        Ok(())
-    }
+	pub fn shutdown(&self) -> Result<(), Error> {
+		Ok(())
+	}
 }
